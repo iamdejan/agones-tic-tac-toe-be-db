c9bc7a3f164e20f72bb70db8c82e0dcaba50e4ad
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-06-19 22:19:27 +0700

    Add connection to database

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import express from "express";
 import http from "http2";
+import mongoose from "mongoose";
 
 const app = express();
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send("Hello world");
+app.get("/healthcheck", (req, res) => {
+  res.send({ status: "UP" });
 });
 
+const username = "test";
+const password = "test";
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@agonestictactoe.0deyc.mongodb.net/?retryWrites=true&w=majority`,
+  () => {
+    console.log("connected to MongoDB");
+  }
+);
+
 app.listen(3002, () => {
-  console.log('listening on *:3002');
+  console.log("listening on *:3002");
 });
