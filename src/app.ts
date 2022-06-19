f014a5b787c75a54cfdc9280e1aa2f3cace7cbc0
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-06-19 23:10:15 +0700

    add cors; fix nanoid version; add log

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import http from "http2";
 import handleRoutes from "./handlers";
@@ -6,6 +7,7 @@ import connectToDatabase from "./utils/mongo.utility";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
 const server = http.createServer(app);
 
--- src/handlers.ts
+++ src/handlers.ts
@@ -1,21 +1,24 @@
-import { Express, Request, Response } from "express";
+import { Express } from 'express-serve-static-core';
+import { Request, Response } from "express";
 import { nanoid } from "nanoid";
 import GameSchema from "./models/GameSchema";
 import Game from "./types/Game";
-
-export default function handleRoutes(app: Express) {
-  app.get("/healthcheck", healthcheck);
-
-  app.post("/games", createGame);
-}
 
 function healthcheck(request: Request, response: Response) {
   response.json({"status": "UP"});
 }
 
 function createGame(request: Request, response: Response) {
-  const game: Game = {id: nanoid(), serverUrl: request.body["serverUrl"]};
+  const serverUrl: string = request.body["serverUrl"];
+  const game: Game = { id: nanoid(), serverUrl };
   GameSchema.create(game).then(() => {
+    console.log(`game is created with id ${game.id}`);
     response.send({gameId: game.id});
   });
+}
+
+export default function handleRoutes(app: Express) {
+  app.get("/healthcheck", healthcheck);
+
+  app.post("/games", createGame);
 }
